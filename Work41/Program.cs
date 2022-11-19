8be49ad4d80bddd814ad8cf6941f1a2e862d9d9e
DenisIG333 <ignatovdnc@inbox.ru>
2022-11-19 10:53:54 +0300

    Домашнее задание

---
+++ Work41/Program.cs
@@ -0,0 +1,36 @@
+﻿/* 
+Задача 41: Пользователь вводит с клавиатуры M чисел.
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3 
+*/
+
+int GetNumber(string message)
+{
+    int numberMoreThan0 = 0;
+    int countNumbers = 0;
+    int result = 0;
+
+    while(countNumbers < 5)
+    {
+        Console.WriteLine(message);
+        result = int.Parse(Console.ReadLine());
+        countNumbers++;
+        
+        if(result > 0)
+        {
+            numberMoreThan0++;
+        }
+    }
+
+    Console.WriteLine($"Чисел больше 0 -> {numberMoreThan0}");
+    return result;
+    
+}
+
+int number = GetNumber("Введите число");
+
+
+
